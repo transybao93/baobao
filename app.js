8cdba1b69103477248eb8ff64839664b5551482b
transybao <IT_coder_93@yahoo.com.vn>
2015-07-22 16:42:03 +0700

    2
    2

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	//io = require('socket.io').listen(server);
 	var port = Number(process.env.PORT || 3000);
 server.listen(port);
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/UI.html');
