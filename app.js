10997c53ffd39de15ec495afe7bd66b54f326468
transybao <IT_coder_93@yahoo.com.vn>
2015-07-23 00:23:06 +0700

    xzxc
    zxczxc

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
+	nicknames = [];
 	var port = process.env.PORT || 3000;
 server.listen(port);
 // io.configure(function () { 
@@ -15,8 +16,22 @@ app.get('/', function(req, res){
 
 //server nhận data từ client
 io.sockets.on('connection', function(socket){
+	//nhận data new user
+	socket.on('new user', function(data, callback){
+		if(nicknames.indexOf(data) != -1)
+		{
+			callback(false);
+		}else{
+			callback(true);
+			socket.nickname = data;
+			nickname.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+		}
+	});
+	//nhận thông tin từ client
 	socket.on('send mess', function(data)
 	{
+		//send data lại cho client
 		io.sockets.emit('new mess', data);
 		
 	});
