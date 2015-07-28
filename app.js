de7496f0b81edc821b41eac45ff1c441f1e8999e
transybao <IT_coder_93@yahoo.com.vn>
2015-07-28 10:43:16 +0700

    change some code and adding hashtag

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	nicknames = [];
+	users = {};
 	var port = process.env.PORT || 3000;
 server.listen(port);
 // io.configure(function () { 
@@ -10,48 +10,76 @@ server.listen(port);
 //   io.set("polling duration", 10); 
 // });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res)
+{
 	res.sendFile(__dirname + '/UI.html');
 });
 
 //server nhận data từ client
 io.sockets.on('connection', function(socket){
-	//send về list nick ban đầu
-	if(nicknames.length != 0)
-	{
-		updateNickName();
-	}
 	
 
 	//nhận data new user
 	socket.on('new user', function(data, callback){
-		if(nicknames.indexOf(data) != -1)
+		if(data in users)
 		{
 			callback(false);
 		}else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
-			//gửi giá trị xuống client để hiển thị
-			io.sockets.emit('current user', data);
+			users[socket.nickname] = socket;
 			updateNickName();
 		}
 	});
 
+
 	function updateNickName()
 	{
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 	//nhận thông tin từ client
-	socket.on('send mess', function(data)
+	socket.on('send mess', function(data, callback)
 	{
-		//send data lại cho client
-		io.sockets.emit('new mess', {msg: data, nick: socket.nickname});
+
+		// var datetime = new Date().today() + " @ " + new Date().timeNow();
+		//io.sockets.emit('new mess', {msg: data, nick: socket.nickname});
+		var msg = data.trim();
+		if(msg.substr(0,1) === '#')
+		{
+			//ind = #bao_
+			var ind = msg.indexOf(' ');
+			if(ind != -1)
+			{
+				var name = msg.substr(1, ind-1);
+				var msg = msg.substring(ind + 1);
+				console.log('Check index');
+				console.log(ind + ' - ' + name + ' : ' + msg);
+				if(name in users)
+				{
+					console.log('Hashtag');
+					console.log(ind + ' - ' + name + ' : ' + msg);
+					//send private
+					users[name].emit('hashtag', {msg: msg, nick: socket.nickname});
+					//send public
+					io.sockets.emit('new mess', {msg: data, nick: socket.nickname});
+				}else{
+					callback('Lỗi : Không tồn tại người dùng này');
+				}
+				
+			}else{
+				console.log('Error');
+				callback('Lỗi : Vui lòng nhập nội dung');
+			}
+		}else{
+			//send data lại cho client
+			io.sockets.emit('new mess', {msg: msg, nick: socket.nickname});
+		}
+		
 	});
 
 	socket.on('disconnect', function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		delete users[socket.nickname];
 		updateNickName();
 	});
 });
